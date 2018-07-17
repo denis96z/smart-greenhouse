3ce96e6d76a631acfff4d27f31125344b25af5a5
denis96z <zinoviev.denis@yandex.ru>
2018-07-17 21:11:28 +0300

    Refactoring

--- server/src/main/java/com/netcracker/edu/smartgreenhouseservice/SmartGreenhouseServiceApplication.java
+++ server/src/main/java/com/netcracker/edu/smartgreenhouseservice/SmartGreenhouseServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartGreenhouseServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SmartGreenhouseServiceApplication.class, args);
     }
