5e9ef0fb83f50bb89bf6d6a9cd2f5ee9a66a8c56
denis96z <zinoviev.denis@yandex.ru>
2018-07-17 20:53:03 +0300

    Server: init files added

---
+++ server/src/main/java/com/netcracker/edu/smartgreenhouseservice/SmartGreenhouseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker.edu.smartgreenhouseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGreenhouseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGreenhouseServiceApplication.class, args);
+    }
+}
