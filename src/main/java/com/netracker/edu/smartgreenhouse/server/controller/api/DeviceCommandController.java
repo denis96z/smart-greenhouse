609b38e5ea015c513a3b1be3a817b7ee941c387e
denis96z <zinoviev.denis@yandex.ru>
2018-07-27 18:56:35 +0300

    Refactoring

--- src/main/java/com/netracker/edu/smartgreenhouse/server/controller/api/DeviceCommandController.java
+++ src/main/java/com/netracker/edu/smartgreenhouse/server/controller/api/DeviceCommandController.java
@@ -1,15 +1,21 @@
 package com.netracker.edu.smartgreenhouse.server.controller.api;
 
 import com.netracker.edu.smartgreenhouse.server.domain.DeviceCommand;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+
+import static com.netracker.edu.smartgreenhouse.server.constant.Format.DATE_FORMAT;
 
 @RestController
 @RequestMapping("/api/commands")
 public class DeviceCommandController {
     @GetMapping("/{deviceId}/all")
-    public List<DeviceCommand> getAllDeviceCommands(@PathVariable Long deviceId) {
+    public List<DeviceCommand> getDeviceCommands(@PathVariable Long deviceId,
+            @RequestParam("fromDate") @DateTimeFormat(pattern = DATE_FORMAT) Date fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = DATE_FORMAT) Date toDate) {
         throw new RuntimeException();
     }
 
@@ -19,12 +25,16 @@ public class DeviceCommandController {
     }
 
     @GetMapping("/{deviceId}/executed/ok")
-    public List<DeviceCommand> getExecutedOkDeviceCommands(@PathVariable Long deviceId) {
+    public List<DeviceCommand> getExecutedOkDeviceCommands(@PathVariable Long deviceId,
+            @RequestParam("fromDate") @DateTimeFormat(pattern = DATE_FORMAT) Date fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = DATE_FORMAT) Date toDate) {
         throw new RuntimeException();
     }
 
     @GetMapping("/{deviceId}/executed/error")
-    public List<DeviceCommand> getExecutedErrorDeviceCommands(@PathVariable Long deviceId) {
+    public List<DeviceCommand> getExecutedErrorDeviceCommands(@PathVariable Long deviceId,
+            @RequestParam("fromDate") @DateTimeFormat(pattern = DATE_FORMAT) Date fromDate,
+            @RequestParam("toDate") @DateTimeFormat(pattern = DATE_FORMAT) Date toDate) {
         throw new RuntimeException();
     }
 
