9f1cdfb299d4fee36661ad580bd8d0e95d02b982
denis96z <zinoviev.denis@yandex.ru>
2018-07-29 23:41:57 +0300

    Freemarker -> Thymeleaf

--- src/main/java/com/netracker/edu/smartgreenhouse/server/controller/web/IndexController.java
+++ src/main/java/com/netracker/edu/smartgreenhouse/server/controller/web/IndexController.java
@@ -2,11 +2,12 @@ package com.netracker.edu.smartgreenhouse.server.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String getIndexPage() {
-        return "index";
+    public ModelAndView getIndexPage() {
+        return new ModelAndView("layout");
     }
 }
--- src/main/resources/templates/index.ftl
+++
@@ -1,5 +0,0 @@
-<#include "layout.ftl">
-
-<#macro page_title>Главная</#macro>
-
-<@page_layout/>
