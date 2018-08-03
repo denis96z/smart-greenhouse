0768b593ba4ce667f48e0e38e5b65d470d334dba
denis96z <zinoviev.denis@yandex.ru>
2018-08-03 21:12:05 +0300

    Some domains: Java -> Kotlin

---
+++ src/main/kotlin/com/netracker/edu/smartgreenhouse/server/domain/Device.kt
@@ -0,0 +1,17 @@
+package com.netracker.edu.smartgreenhouse.server.domain
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+data class Device(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: UUID? = null,
+
+    var aliasName: String? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    var greenhouse: Greenhouse? = null
+)
