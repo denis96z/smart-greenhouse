d66cb348fa336688e2bb611f6031b78123293b60
denis96z <zinoviev.denis@yandex.ru>
2018-08-06 14:27:44 +0300

    Domain 'Person' modified

--- src/main/kotlin/com/netracker/edu/smartgreenhouse/server/domain/Person.kt
+++ src/main/kotlin/com/netracker/edu/smartgreenhouse/server/domain/Person.kt
@@ -1,15 +1,17 @@
 package com.netracker.edu.smartgreenhouse.server.domain
 
-import java.util.*
 import javax.persistence.*
 
 @Entity
 data class Person(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: UUID? = null,
+    var id: Long? = null,
+
+    var username: String? = null,
+    var password: String? = null,
 
     var firstName: String? = null,
     var lastName: String? = null,
-    var username: String? = null
+    var email: String? = null
 )
