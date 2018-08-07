0e8e2ffcb8c3f4462fa7103454def769c3433867
denis96z <zinoviev.denis@yandex.ru>
2018-08-07 17:42:32 +0300

    DeviceController methods implemented

--- src/main/java/com/netracker/edu/smartgreenhouse/server/controller/api/DeviceController.java
+++ src/main/java/com/netracker/edu/smartgreenhouse/server/controller/api/DeviceController.java
@@ -1,36 +1,54 @@
 package com.netracker.edu.smartgreenhouse.server.controller.api;
 
 import com.netracker.edu.smartgreenhouse.server.domain.Device;
-import com.netracker.edu.smartgreenhouse.server.exception.NotImplementedException;
+import com.netracker.edu.smartgreenhouse.server.exception.NotFoundException;
+import com.netracker.edu.smartgreenhouse.server.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/devices")
 public class DeviceController {
+    private final DeviceService service;
+
+    @Autowired
+    public DeviceController(DeviceService service) {
+        this.service = service;
+    }
+
     @PostMapping("/new")
     public void addDeviceInfo(@RequestBody Device device) {
-        throw new NotImplementedException();
+        service.addDeviceInfo(device);
     }
 
     @GetMapping("{deviceId}")
-    public void getDeviceInfo(@PathVariable UUID deviceId) {
-        throw new NotImplementedException();
+    public Device getDeviceInfo(@PathVariable UUID deviceId) {
+        return service.getDeviceInfo(deviceId);
     }
 
     @PutMapping("{deviceId}")
     public void editDeviceInfo(@PathVariable UUID deviceId, @RequestBody Device device) {
-        throw new NotImplementedException();
+        device.setId(deviceId);
+        service.editDeviceInfo(device);
     }
 
     @DeleteMapping("{deviceId}")
     public void deleteDeviceInfo(@PathVariable UUID deviceId) {
-        throw new NotImplementedException();
+        service.deleteDeviceInfo(deviceId);
     }
 
     @GetMapping("/greenhouse/{greenhouseId}")
-    public void getDevicesByGreenhouse(@PathVariable UUID greenhouseId) {
-        throw new NotImplementedException();
+    public List<Device> getDevicesByGreenhouse(@PathVariable UUID greenhouseId) {
+        return service.getDevicesByGreenhouse(greenhouseId);
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public String handleNotFoundException(NotFoundException exception) {
+        return exception.getMessage();
     }
 }
