73154a8e23bdbe8f99d5ccafc6ed687ba434ee52
denis96z <zinoviev.denis@yandex.ru>
2018-08-07 23:50:09 +0300

    Refactoring

--- src/main/java/com/netracker/edu/smartgreenhouse/server/repository/PersonRepository.java
+++ src/main/java/com/netracker/edu/smartgreenhouse/server/repository/PersonRepository.java
@@ -3,8 +3,6 @@ package com.netracker.edu.smartgreenhouse.server.repository;
 import com.netracker.edu.smartgreenhouse.server.domain.Person;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
-
 public interface PersonRepository extends CrudRepository<Person, Long> {
     Person findByUsername(String username);
 }
