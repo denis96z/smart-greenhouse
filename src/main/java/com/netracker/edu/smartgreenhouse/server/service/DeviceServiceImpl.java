234df461916a9a757f592c742d0251c524301ab4
denis96z <zinoviev.denis@yandex.ru>
2018-08-07 17:39:47 +0300

    DeviceService methods implemented

--- src/main/java/com/netracker/edu/smartgreenhouse/server/service/DeviceServiceImpl.java
+++ src/main/java/com/netracker/edu/smartgreenhouse/server/service/DeviceServiceImpl.java
@@ -1,36 +1,61 @@
 package com.netracker.edu.smartgreenhouse.server.service;
 
 import com.netracker.edu.smartgreenhouse.server.domain.Device;
-import com.netracker.edu.smartgreenhouse.server.exception.NotImplementedException;
+import com.netracker.edu.smartgreenhouse.server.exception.NotFoundException;
+import com.netracker.edu.smartgreenhouse.server.repository.DeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class DeviceServiceImpl implements DeviceService {
+    private final DeviceRepository deviceRepository;
+
+    @Autowired
+    public DeviceServiceImpl(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
+
     @Override
     public void addDeviceInfo(Device device) {
-        throw new NotImplementedException();
+        deviceRepository.save(device);
     }
 
     @Override
     public Device getDeviceInfo(UUID deviceId) {
-        throw new NotImplementedException();
+        var device = deviceRepository.findById(deviceId);
+        if (device.isPresent()) {
+            return device.get();
+        }
+        throw new NotFoundException("Device not found");
     }
 
     @Override
     public void editDeviceInfo(Device device) {
-        throw new NotImplementedException();
+        var newDevice = deviceRepository.findById(device.getId());
+        if (newDevice.isPresent()) {
+            deviceRepository.save(device);
+        }
+        throw new NotFoundException("Device not found");
     }
 
     @Override
     public void deleteDeviceInfo(UUID deviceId) {
-        throw new NotImplementedException();
+        var device = deviceRepository.findById(deviceId);
+        if (device.isPresent()) {
+            deviceRepository.delete(device.get());
+        } else {
+            throw new NotFoundException("Device not found");
+        }
     }
 
     @Override
     public List<Device> getDevicesByGreenhouse(UUID greenhouseId) {
-        throw new NotImplementedException();
+        var list = new ArrayList<Device>();
+        deviceRepository.findByGreenhouse_Id(greenhouseId).forEach(list::add);
+        return list;
     }
 }
