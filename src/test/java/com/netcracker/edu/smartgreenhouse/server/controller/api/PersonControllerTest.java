36e4aef317414907a592771a3212f0514c4f91ee
denis96z <zinoviev.denis@yandex.ru>
2018-08-15 23:01:26 +0300

    Some PersonController tests added

--- src/test/java/com/netcracker/edu/smartgreenhouse/server/controller/api/PersonControllerTest.java
+++ src/test/java/com/netcracker/edu/smartgreenhouse/server/controller/api/PersonControllerTest.java
@@ -2,19 +2,19 @@ package com.netcracker.edu.smartgreenhouse.server.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netcracker.edu.smartgreenhouse.server.domain.Person;
+import com.netcracker.edu.smartgreenhouse.server.exception.NotFoundException;
 import com.netcracker.edu.smartgreenhouse.server.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = PersonController.class, secure = false)
@@ -26,16 +26,70 @@ public class PersonControllerTest {
     private PersonService personService;
 
     @Test
-    public void addPerson() throws Exception {
+    public void addPersonInfoReturnsOkOnCorrectData() throws Exception {
         var person = new Person();
-
-        when(personService.addPersonInfo(person)).thenReturn(person);
 
         var objMapper = new ObjectMapper();
         var strPerson = objMapper.writeValueAsString(person);
 
         mockMvc
-                .perform(post("/api/persons/new").contentType(MediaType.APPLICATION_JSON).content(strPerson))
-                .andExpect(status().isOk());
+                .perform(MockMvcRequestBuilders
+                        .post("/api/persons/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(strPerson))
+                .andExpect(MockMvcResultMatchers
+                        .status().isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content().string(""));
+    }
+
+    @Test
+    public void addPersonInfoReturnsBadRequestOnWrongData() throws Exception {
+        var strNotPerson = "";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/persons/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(strNotPerson))
+                .andExpect(MockMvcResultMatchers
+                        .status().isBadRequest())
+                .andExpect(MockMvcResultMatchers
+                        .content().string(""));
+    }
+
+    @Test
+    public void getPersonInfoReturnsOkOnExistingPerson() throws Exception {
+        var person = new Person();
+        var personId = Long.valueOf(1);
+
+        var objMapper = new ObjectMapper();
+        var strPerson = objMapper.writeValueAsString(person);
+
+        Mockito.when(personService.getPersonInfo(personId)).thenReturn(person);
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/api/persons/" + personId))
+                .andExpect(MockMvcResultMatchers
+                        .status().isOk())
+                .andExpect(MockMvcResultMatchers
+                        .content().string(strPerson));
+    }
+
+    @Test
+    public void getPersonInfoReturnsNotFoundOnNonExistingPerson() throws Exception {
+        var personId = Long.valueOf(1);
+
+        var errMsg = "Person not found";
+        Mockito.when(personService.getPersonInfo(personId))
+                .thenThrow(new NotFoundException(errMsg));
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/api/persons/" + personId))
+                .andExpect(MockMvcResultMatchers
+                        .status().isNotFound())
+                .andExpect(MockMvcResultMatchers
+                        .content().string(errMsg));
     }
 }
