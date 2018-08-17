a13445a58ed015ddba6c6a1c9c71e3baf19751ce
denis96z <zinoviev.denis@yandex.ru>
2018-08-17 19:19:22 +0300

    GreenhouseServiceImpl tests added

--- src/main/java/com/netcracker/edu/smartgreenhouse/server/service/GreenhouseService.java
+++ src/main/java/com/netcracker/edu/smartgreenhouse/server/service/GreenhouseService.java
@@ -1,6 +1,7 @@
 package com.netcracker.edu.smartgreenhouse.server.service;
 
 import com.netcracker.edu.smartgreenhouse.server.domain.Greenhouse;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
@@ -8,17 +9,17 @@ import java.util.UUID;
 
 public interface GreenhouseService {
     @Secured("ROLE_ADMIN")
-    void addGreenhouseInfo(Greenhouse greenhouse);
+    Greenhouse addGreenhouseInfo(@NotNull Greenhouse greenhouse);
 
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
-    Greenhouse getGreenhouseInfo(UUID greenhouseId);
+    Greenhouse getGreenhouseInfo(@NotNull UUID greenhouseId);
 
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
-    void editGreenhouseInfo(Greenhouse greenhouse);
+    Greenhouse editGreenhouseInfo(@NotNull Greenhouse greenhouse);
 
     @Secured("ROLE_ADMIN")
-    void deleteGreenhouseInfo(UUID greenhouseId);
+    Greenhouse deleteGreenhouseInfo(@NotNull UUID greenhouseId);
 
     @Secured("ROLE_USER")
-    List<Greenhouse> getGreenhousesByOwner(Long ownerId);
+    List<Greenhouse> getGreenhousesByOwner(@NotNull Long ownerId);
 }
